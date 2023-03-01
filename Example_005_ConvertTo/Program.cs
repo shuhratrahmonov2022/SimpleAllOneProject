ad76f42252b3166269d66e449629c9e8da1673f9
Shuhrat2022 <shuhratrahmonov2022@gmail.com>
2023-03-01 17:21:27 +0500

    finished

---
+++ Example_005_ConvertTo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Example_005_ConvertTo
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string inputString = "2029";
+
+            int intValue = Convert.ToInt32(inputString);
+            Console.WriteLine(intValue);
+
+            double dubleValue = Convert.ToDouble(inputString);
+            Console.WriteLine(dubleValue);
+
+            ulong ulongValue = Convert.ToUInt64(inputString);
+            Console.WriteLine(ulongValue);
+
+            #region Типы данных и их синонимы
+
+            //Тип данных Тип .NET
+            //sbyte  System.SByte
+            //short  System.Int16
+            //int    System.Int32
+            //long   System.Int64
+            //byte   System.Sbyte
+            //ushort System.UInt16
+            //uint   System.Uint32
+            //ulong  System.UInt64
+            //float  System.Single
+            //doubl  System.Double
+            //bool   System.Boolean
+
+            #endregion  
+        }
+    }
+}
