28ae0e3e69c6c862a88a09c63f9b3af260a19619
Shuhrat2022 <shuhratrahmonov2022@gmail.com>
2023-03-01 17:12:44 +0500

    fix errors some

---
+++ Example_004_ParseConversion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Example_004_ParseConversion
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string inputString = "2029";
+            int intValue = int.Parse(inputString);
+            Console.WriteLine(intValue);
+
+            double doubleValue = double.Parse(inputString);
+            Console.WriteLine(doubleValue);
+
+            ulong ulongValue = ulong.Parse(inputString);
+            Console.WriteLine(ulongValue);
+
+            byte byteValue = byte.Parse(inputString);
+            Console.WriteLine(byteValue);
+
+        }
+    }
+}
